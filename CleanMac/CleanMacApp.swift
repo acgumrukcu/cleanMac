f553a2db0550d34abd858be50fc396b3d3a196c9
acgumrukcu <a.c.gumrukcu@gmail.com>
2025-03-21 16:25:05 +0300

    Initial Commit

---
+++ CleanMac/CleanMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMacApp.swift
+//  CleanMac
+//
+//  Created by Ahmet Can Gümrükçü on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanMac
+//
+//  Created by Ahmet Can Gümrükçü on 21.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
